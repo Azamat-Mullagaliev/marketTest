14369ccedca6c1c7748ab346c7c4f7bc8ad40ed2
Azamat Mullagaliev <Azamat_Mullagaliev@users.noreply.github.com>
2018-04-11 12:00:05 +0500

    Добавил комментарии

--- src/test/java/com/mytest/webdriver/FirstAutomationTest.java
+++ src/test/java/com/mytest/webdriver/FirstAutomationTest.java
@@ -11,21 +11,21 @@ import org.openqa.selenium.NoSuchElementException;
 
 public class FirstAutomationTest {
 
-    public void selectPhone(WebDriver driver) throws InterruptedException {
-        driver.findElement(By.className("n-product-tabs__item_name_offers")).click();
-        driver.findElement(By.linkText("по цене")).click();
-        Thread.sleep(2000);
+    public void selectPhone(WebDriver driver) throws InterruptedException { //выдача информации по найденному телефону
+        driver.findElement(By.className("n-product-tabs__item_name_offers")).click(); //переход к ценам
+        driver.findElement(By.linkText("по цене")).click(); //сортировка по цене
+        Thread.sleep(2000); //время на прогрузку
 
-        String phoneShop = driver.findElement(By.className("n-snippet-card__shop")).findElement(By.className("link")).getText();
-        String phonePrice = driver.findElement(By.className("snippet-card__price")).getText();
+        String phoneShop = driver.findElement(By.className("n-snippet-card__shop")).findElement(By.className("link")).getText(); //магазин
+        String phonePrice = driver.findElement(By.className("snippet-card__price")).getText();//цена
 
-        driver.findElement(By.className("snippet-card__image")).click();
+        driver.findElement(By.className("snippet-card__image")).click();//переход в магазин
 
         System.out.println("магазин: " + phoneShop);
         System.out.println("цена: " + phonePrice);
     }
 
-    public void searchFromList(WebDriver driver, ArrayList<WebElement> phones, String phoneName) throws InterruptedException {
+    public void searchFromList(WebDriver driver, ArrayList<WebElement> phones, String phoneName) throws InterruptedException { //поиск нужной модели в списке
         for (WebElement phone : phones)
         {
             //System.out.println("ищем...");
@@ -44,18 +44,18 @@ public class FirstAutomationTest {
         System.setProperty("webdriver.chrome.driver", "C:/Users/azama/Downloads/chromedriver/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get("https://market.yandex.ru/");
-        String phoneName = "LG Q6+";
-        driver.findElement(By.className("input__control")).sendKeys(phoneName);
+        String phoneName = "LG Q6";
+        driver.findElement(By.className("input__control")).sendKeys(phoneName); //ввод названия телефона
         driver.findElement(By.className("search2__button")).click();
 
         try {
-            driver.findElement(By.className("n-noresult__desc"));
+            driver.findElement(By.className("n-noresult__desc")); //для случая, если ничего не найдено
         } catch (NoSuchElementException e1) {
             try {
-                String foundPhone = driver.findElement(By.className("n-title__text")).getText();
+                String foundPhone = driver.findElement(By.className("n-title__text")).getText(); //для сценария, когда маркет сразу предлагает определенную модель. Для данного случая это Q6+ вместо запрашиваемого Q6
                 if (foundPhone.contains(" " + phoneName + " ")||foundPhone.contains(phoneName + " ")||foundPhone.endsWith(phoneName)) //проверка конкретной модификации
                     selectPhone(driver);
-                else {
+                else { // на случай, если маркет предложил неправильно
                     //System.out.println("ищем телефон");
                     driver.findElement(By.className("n-search-preciser__text")).findElement(By.className("link")).click();
                     Thread.sleep(2000);
@@ -66,13 +66,12 @@ public class FirstAutomationTest {
                     searchFromList(driver,phones,phoneName);
  
                 }
-            } catch (NoSuchElementException e2) {
+            } catch (NoSuchElementException e2) { //на случай, если маркет выдал список телефонов
                 ArrayList<WebElement> phones = new ArrayList<WebElement>(driver.findElements(By.className("n-snippet-cell2__header")));
                 searchFromList(driver,phones,phoneName);
             }
 
     }
-
         Thread.sleep(20000);
         driver.quit();
     }
